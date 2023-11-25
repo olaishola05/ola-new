83ae5ca54cccee194aaa6661d028dd7f5a3d5005
Oladipupo Ishola <olaishola@hotmail.co.uk>
2023-11-25 20:20:34 +0100

    feat(app): A bit of refactoring

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,44 +1,37 @@
-// import { NextResponse, NextRequest } from "next/server"
-
-// export default async function middleware(req: NextRequest) {
-//   const isLoggedIn = req.cookies?.getAll()?.some(cookie => cookie.name === 'next-auth.session-token') ?? false;
-//   const isOnAdminPage = req.nextUrl?.pathname.startsWith('/admin');
-//   console.log('isLoggedIn', isLoggedIn)
-//   if (isOnAdminPage) {
-//     if (isLoggedIn) return NextResponse.next()
-//     return NextResponse.redirect(new URL('/auths/signin', req?.nextUrl))
-//   } else if (isLoggedIn) {
-//     return NextResponse.redirect(new URL('/admin', req.nextUrl))
-//   }
-//   return NextResponse.next()
-// }
-
-
-// export const config = {
-//   matcher: ['/admin/:path*',],
-// }
-
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server"
 
 export default async function middleware(req: NextRequest) {
   const isLoggedIn = req.cookies?.getAll()?.some(cookie => cookie.name === 'next-auth.session-token') ?? false;
   const isOnAdminPage = req.nextUrl?.pathname.startsWith('/admin');
-
   if (isOnAdminPage) {
-    if (isLoggedIn) {
-      return NextResponse.next();
-    } else {
-      const redirectUrl = new URL('/auths/signin', req.nextUrl.origin);
-      redirectUrl.search = `redirect=${encodeURIComponent(req.nextUrl.pathname)}`;
-      return NextResponse.redirect(redirectUrl);
-    }
+    if (isLoggedIn) return NextResponse.next()
+    return NextResponse.redirect(new URL('/auths/signin', req?.nextUrl.origin))
   } else if (isLoggedIn) {
-    return NextResponse.redirect(new URL('/admin', req.nextUrl.origin));
+    console.log('redirect to admin')
+    return NextResponse.redirect(new URL('/admin/dashboard', req.nextUrl.origin))
   }
-
-  return NextResponse.next();
+  return NextResponse.next()
 }
 
+
 export const config = {
-  matcher: ['/admin/:path*'],
-};
+  matcher: ['/admin/:path*',],
+}
+
+// import { NextResponse, NextRequest } from "next/server";
+
+// export default async function middleware(req: NextRequest) {
+//   const isLoggedIn = req.cookies?.getAll()?.some(cookie => cookie.name === 'next-auth.session-token') ?? false;
+//   if (isLoggedIn) {
+//     const adminRedirect = new URL('/admin/dashboard', req.nextUrl.origin);
+//     return NextResponse.redirect(adminRedirect);
+//   } else {
+//     const redirectUrl = new URL('/auths/signin', req.nextUrl.origin);
+//     redirectUrl.search = `redirect=${encodeURIComponent(req.nextUrl.pathname)}`;
+//     return NextResponse.redirect(redirectUrl);
+//   }
+// }
+
+// export const config = {
+//   matcher: ['/admin/:path*'],
+// };
