dbe80fb31335388d8cceeabc827caa7901895b88
Oladipupo Ishola <olaishola@hotmail.co.uk>
2023-11-28 15:52:15 +0100

    feat(app): Fix build error with metadata name error

--- src/app/auths/signin/page.tsx
+++ src/app/auths/signin/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 import { Metadata } from 'next'
 import LoginButton from '@/components/auths/LoginButton'
 
-export const meta: Metadata = {
+export const metadata: Metadata = {
   title: 'Welcome back! | Sign In',
   description: 'Sign In page',
 }
