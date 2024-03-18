f724eabf8047314934f1fb3de7bfaace27131e50
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-03-18 19:28:04 +0100

    feat(app): Update dashboard nav links and layout

--- src/app/admin/dashboard/DashBoardSidebar.tsx
+++ src/app/admin/dashboard/DashBoardSidebar.tsx
@@ -2,16 +2,20 @@
 
 import React from 'react'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation';
+import { usePathname, redirect } from 'next/navigation';
 import { signOut } from 'next-auth/react';
 
 const Circle = (isActive: (pathname: string) => boolean, pathname: string) => (
   <div className={`w-2 h-2 rounded-full ${isActive(pathname) && 'bg-blue-400'}`}></div>
 )
 
-export default function DashBoardSidebar() {
+export default function DashBoardSidebar({ session }: any) {
   const pathname = usePathname();
   const isActive = (path: string) => path === pathname;
+
+  if (!session?.user?.role.includes('admin') && !session?.user?.role.includes('author')) {
+    redirect('/')
+  }
 
   const handleLogout = async () => {
     await signOut({ redirect: true, callbackUrl: '/' })
@@ -20,25 +24,38 @@ export default function DashBoardSidebar() {
   return (
     <div className='flex flex-col gap-5 mt-5 mx-auto'>
       <div className='flex gap-2 items-center'>
+        {Circle(isActive, '/')}
+        <Link href='/admin/dashboard' className='text-base text-[var(--textColor)]'>Home</Link>
+      </div>
+      <div className='flex gap-2 items-center'>
         {Circle(isActive, '/admin/dashboard')}
-        <Link href='/admin/dashboard' className='text-base text-[var(--textColor)]'>Dashboard</Link>
+        <Link href='/admin/dashboard' className='text-base text-[var(--textColor)]'>Analytics</Link>
       </div>
-      <div className='flex gap-2 items-center'>
-        {Circle(isActive, '/admin/dashboard/projects')}
-        <Link href='/admin/dashboard/projects' className='text-base text-[var(--textColor)]'>Projects</Link>
-      </div>
-      <div className='flex gap-2 items-center'>
-        {Circle(isActive, '/admin/dashboard/create')}
-        <Link href='/admin/dashboard/create' className='text-base text-[var(--textColor)]'>Add Project</Link>
-      </div>
-      <div className='flex gap-2 items-center'>
-        {Circle(isActive, '/admin/dashboard/posts')}
-        <Link href="/admin/dashboard/posts" className='text-base text-[var(--textColor)]'>Posts</Link>
-      </div>
-      <div className='flex gap-2 items-center'>
-        {Circle(isActive, '/admin/dashboard/write')}
-      </div>
-      <span className='cursor-pointer text-lg capitalize' onClick={handleLogout}>Logout</span>
+      {session?.user?.role.includes('admin') && (<>
+        <div className='flex gap-2 items-center'>
+          {Circle(isActive, '/admin/dashboard/projects')}
+          <Link href='/admin/dashboard/projects' className='text-base text-[var(--textColor)]'>Projects</Link>
+        </div>
+        <div className='flex gap-2 items-center'>
+          {Circle(isActive, '/admin/dashboard/create')}
+          <Link href='/admin/dashboard/create' className='text-base text-[var(--textColor)]'>Add Project</Link>
+        </div>
+      </>)}
+      <>
+        <div className='flex gap-2 items-center'>
+          {Circle(isActive, '/admin/dashboard/posts')}
+          <Link href="/admin/dashboard/posts" className='text-base text-[var(--textColor)]'>All Posts</Link>
+        </div>
+        <div className='flex gap-2 items-center'>
+          {Circle(isActive, '/admin/dashboard/posts/drafts')}
+          <Link href="/admin/dashboard/posts/drafts" className='text-base text-[var(--textColor)]'>Draft Posts</Link>
+        </div>
+        <div className='flex gap-2 items-center'>
+          {Circle(isActive, '/admin/dashboard/posts/published')}
+          <Link href="/admin/dashboard/posts/published" className='text-base text-[var(--textColor)]'>Published Posts</Link>
+        </div>
+      </>
+      <span className='cursor-pointer text-base capitalize' onClick={handleLogout}>Logout</span>
     </div>
   )
 }
--- src/app/admin/dashboard/layout.tsx
+++ src/app/admin/dashboard/layout.tsx
@@ -9,15 +9,15 @@ interface Props {
 export default async function DashboardLayout({ children }: Props) {
   const session = await getAuthSession();
   return (
-    <div className='flex justify-between mt-10 w-full gap-3'>
-      <section className='flex w-1/5'>
-        <div className='bg-[#F2F6FF] flex flex-col gap-20 w-full px-2'>
-          <h1 className='text-2xl text-center pt-5 font-bold text-[var(--primary)] md:text-4xl'>Ola.</h1>
-          <DashBoardSidebar />
+    <div className='flex justify-between w-screen gap-3'>
+      <section className='flex w-1/5 h-screen'>
+        <div className='bg-bg flex flex-col gap-5 w-full px-2 border-r-2'>
+          <h1 className='text-2xl text-center pt-5 font-bold text-[var(--primary)] md:text-4xl'>Dashboard</h1>
+          <DashBoardSidebar session={session} />
         </div>
       </section>
-      <section className='w-full h-full bg-white relative'>
-        <p className='absolute right-2 top-2'>Welcome {session?.user?.name}</p>
+      <section className='w-[80%] h-full bg-bg relative p-5'>
+        <p className='absolute right-14 top-5'>Hello {session?.user?.name?.split(" ")[0]}!</p>
         {children}
       </section>
     </div>
