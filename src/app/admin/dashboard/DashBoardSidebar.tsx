0e14c6e38b918f9fa48824129faabf4e185e33d6
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-03-19 15:32:59 +0100

    refactor(app): Upadte dashboard and layout
    conditionally render links based on user role

--- src/app/admin/dashboard/DashBoardSidebar.tsx
+++ src/app/admin/dashboard/DashBoardSidebar.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import Link from 'next/link'
 import { usePathname, redirect } from 'next/navigation';
 import { signOut } from 'next-auth/react';
+import Image from 'next/image';
 
 const Circle = (isActive: (pathname: string) => boolean, pathname: string) => (
   <div className={`w-2 h-2 rounded-full ${isActive(pathname) && 'bg-blue-400'}`}></div>
@@ -22,40 +23,62 @@ export default function DashBoardSidebar({ session }: any) {
   }
 
   return (
-    <div className='flex flex-col gap-5 mt-5 mx-auto'>
-      <div className='flex gap-2 items-center'>
-        {Circle(isActive, '/')}
-        <Link href='/admin/dashboard' className='text-base text-[var(--textColor)]'>Home</Link>
+    <aside className='flex flex-col gap-20 mt-5 mx-auto items-center'>
+      <div className='flex flex-col gap-5 mt-10'>
+        <div className='flex gap-2 items-center'>
+          {Circle(isActive, '/')}
+          <Link href='/' className='text-base text-ctaText'>Home</Link>
+        </div>
+        <div className='flex gap-2 items-center'>
+          {Circle(isActive, '/admin/dashboard')}
+          <Link href='/admin/dashboard' className='text-base text-ctaText'>Analytics</Link>
+        </div>
+        {session?.user?.role.includes('admin') && (<>
+          <div className='flex gap-2 items-center'>
+            {Circle(isActive, '/admin/dashboard/projects')}
+            <Link href='/admin/dashboard/projects' className='text-base text-ctaText'>Projects</Link>
+          </div>
+          <div className='flex gap-2 items-center'>
+            {Circle(isActive, '/admin/dashboard/create')}
+            <Link href='/admin/dashboard/create' className='text-base text-ctaText'>Add Project</Link>
+          </div>
+        </>)}
+        <>
+          <div className='flex gap-2 items-center'>
+            {Circle(isActive, '/admin/dashboard/posts')}
+            <Link href="/admin/dashboard/posts" className='text-base text-ctaText'>All Posts</Link>
+          </div>
+          <div className='flex gap-2 items-center'>
+            {Circle(isActive, '/admin/dashboard/posts/drafts')}
+            <Link href="/admin/dashboard/posts/drafts" className='text-base text-ctaText'>Draft Posts</Link>
+          </div>
+          <div className='flex gap-2 items-center'>
+            {Circle(isActive, '/admin/dashboard/posts/published')}
+            <Link href="/admin/dashboard/posts/published" className='text-base text-ctaText'>Published Posts</Link>
+          </div>
+        </>
       </div>
-      <div className='flex gap-2 items-center'>
-        {Circle(isActive, '/admin/dashboard')}
-        <Link href='/admin/dashboard' className='text-base text-[var(--textColor)]'>Analytics</Link>
+      <div className='flex flex-col'>
+        <div className='flex items-center flex-col gap-1'>
+          {session.user.image && (
+            <div className='relative h-[40px] w-10 rounded-full'>
+              <Image
+                src={session.user.image}
+                alt={session.user.name}
+                className='absolute w-10 h-10 rounded-full object-cover'
+                fill
+              />
+            </div>
+          )}
+          <span className='font-bold text-lg text-ctaText'>
+            {session.user.name.split(' ').map((name: string) => name[0]).join('')}
+          </span>
+        </div>
+        <span
+          className='cursor-pointer text-base capitalize mt-auto text-ctaText'
+          onClick={handleLogout}
+        >Logout</span>
       </div>
-      {session?.user?.role.includes('admin') && (<>
-        <div className='flex gap-2 items-center'>
-          {Circle(isActive, '/admin/dashboard/projects')}
-          <Link href='/admin/dashboard/projects' className='text-base text-[var(--textColor)]'>Projects</Link>
-        </div>
-        <div className='flex gap-2 items-center'>
-          {Circle(isActive, '/admin/dashboard/create')}
-          <Link href='/admin/dashboard/create' className='text-base text-[var(--textColor)]'>Add Project</Link>
-        </div>
-      </>)}
-      <>
-        <div className='flex gap-2 items-center'>
-          {Circle(isActive, '/admin/dashboard/posts')}
-          <Link href="/admin/dashboard/posts" className='text-base text-[var(--textColor)]'>All Posts</Link>
-        </div>
-        <div className='flex gap-2 items-center'>
-          {Circle(isActive, '/admin/dashboard/posts/drafts')}
-          <Link href="/admin/dashboard/posts/drafts" className='text-base text-[var(--textColor)]'>Draft Posts</Link>
-        </div>
-        <div className='flex gap-2 items-center'>
-          {Circle(isActive, '/admin/dashboard/posts/published')}
-          <Link href="/admin/dashboard/posts/published" className='text-base text-[var(--textColor)]'>Published Posts</Link>
-        </div>
-      </>
-      <span className='cursor-pointer text-base capitalize' onClick={handleLogout}>Logout</span>
-    </div>
+    </aside>
   )
 }
--- src/app/admin/dashboard/layout.tsx
+++ src/app/admin/dashboard/layout.tsx
@@ -10,14 +10,14 @@ export default async function DashboardLayout({ children }: Props) {
   const session = await getAuthSession();
   return (
     <div className='flex justify-between w-screen gap-3'>
-      <section className='flex w-1/5 h-screen'>
-        <div className='bg-bg flex flex-col gap-5 w-full px-2 border-r-2'>
-          <h1 className='text-2xl text-center pt-5 font-bold text-[var(--primary)] md:text-4xl'>Dashboard</h1>
+      <section className='flex flex-1 h-screen'>
+        <div className='bg-cta flex flex-col gap-5 w-full px-2 shadow-md'>
+          <h1 className='text-2xl text-center pt-5 font-bold text-ctaText md:text-4xl'>Dashboard</h1>
           <DashBoardSidebar session={session} />
         </div>
       </section>
-      <section className='w-[80%] h-full bg-bg relative p-5'>
-        <p className='absolute right-16 top-5'>Hello {session?.user?.name?.split(" ")[0]}!</p>
+      <section className='flex-4 h-full bg-bg relative p-5'>
+        <p className='absolute right-16 top-5 text-textColor'>Hello {session?.user?.name?.split(" ")[0]}!</p>
         {children}
       </section>
     </div>
