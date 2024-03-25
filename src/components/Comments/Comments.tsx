2db9372fbba17ca454bfa963fdf3e7416c640ca0
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-03-25 20:41:22 +0100

    docs: Dynamically set current path to auths/signin search params

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import Link from 'next/link'
 import useSWR from 'swr';
 import { useSession } from 'next-auth/react';
 import Comment from './Comment';
+import { usePathname } from 'next/navigation';
 
 const fetcher = async (url: string) => {
   const res = await fetch(url)
@@ -18,14 +19,26 @@ const fetcher = async (url: string) => {
   return data
 }
 
+interface Item {
+  user: {
+    image: string;
+    name: string;
+  };
+  createdAt: string;
+  desc: string;
+  id: string;
+}
+
 export default function Comments({ postSlug }: { postSlug: string }) {
   const { status } = useSession();
+  const pathname = usePathname();
   const [desc, setDesc] = React.useState('');
+  const [loginUrl, setLoginUrl] = React.useState('');
   const { data, mutate, error, isLoading } = useSWR(`http://localhost:3000/api/v1/comments?postSlug=${postSlug}`, fetcher);
 
   const refinedPostSlug = postSlug.replace(/%20/g, ' ');
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
     const res = await fetch('http://localhost:3000/api/v1/comments', {
       method: 'POST',
@@ -43,6 +56,14 @@ export default function Comments({ postSlug }: { postSlug: string }) {
     mutate();
     setDesc('');
   }
+
+  useEffect(() => {
+    const authUrl = new URL('/auths/signin', window.location.origin);
+    authUrl.searchParams.set('from', pathname);
+    setLoginUrl(authUrl.href);
+  }, [pathname])
+
+
   return (
     <div className='mt-[50px]'>
       <h1 className='text-softText mb-7'>Comments</h1>
@@ -57,10 +78,10 @@ export default function Comments({ postSlug }: { postSlug: string }) {
           <button type="button" className='py-4 px-5 bg-teal-700 text-white font-bold border-none rounded-[5px] cursor-pointer' onClick={handleSubmit}>Send</button>
         </div>
       ) : (
-        <Link href='/login'>Login to write a comment</Link>
+        <Link href={loginUrl}>Login to write a comment</Link>
       )}
 
-      {isLoading ? (<div>loading</div>) : data?.data?.map((item) => (
+      {isLoading ? (<div>loading</div>) : data?.data?.map((item: Item) => (
         <Comment key={item.id} item={item} />
       ))}
 
