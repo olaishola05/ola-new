9d3b42eade3e4d25a3843c297400753be491663b
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-05-13 16:11:42 +0100

    (app): Add greetings to index.js for utils

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,11 +1,40 @@
-import { navItems, socialLinks, randomItemFromArray, tabs, sendDataToBackend, publishProject, deleteProject, updateProject, projectsFilter, resumeTabs, updateAboutInfo, readTimeInfo, blogLinks } from './utilities';
-import { contactSchema, TestimonialSchema } from './validations';
 import {
-  educationItems,
-  workExpItems
-} from './data';
+  navItems,
+  socialLinks,
+  randomItemFromArray,
+  tabs,
+  sendDataToBackend,
+  publishProject,
+  deleteProject,
+  updateProject,
+  projectsFilter,
+  resumeTabs,
+  updateAboutInfo,
+  readTimeInfo,
+  blogLinks,
+} from "./utilities";
+import { contactSchema, TestimonialSchema } from "./validations";
+import { educationItems, workExpItems } from "./data";
 
+export { greetings } from "./utilities";
 
 export {
-  navItems, socialLinks, randomItemFromArray, contactSchema, tabs, sendDataToBackend, publishProject, deleteProject, updateProject, projectsFilter, resumeTabs, updateAboutInfo, educationItems, workExpItems, readTimeInfo, TestimonialSchema, blogLinks
+  navItems,
+  socialLinks,
+  randomItemFromArray,
+  contactSchema,
+  tabs,
+  sendDataToBackend,
+  publishProject,
+  deleteProject,
+  updateProject,
+  projectsFilter,
+  resumeTabs,
+  updateAboutInfo,
+  educationItems,
+  workExpItems,
+  readTimeInfo,
+  TestimonialSchema,
+  blogLinks,
 };
+
