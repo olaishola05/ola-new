7e2365c85e235aee09ccf6411b88e85d786fa8e0
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-05-13 21:21:54 +0100

    refactor(app): update App component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,35 +1,24 @@
-'use client';
+"use client";
 
-import React from 'react'
-import { usePathname } from 'next/navigation'
-import Layout from '../Layouts/Layout'
-import { useSession } from 'next-auth/react';
-import Loader from '../loader/Loader';
+import React from "react";
+import { usePathname } from "next/navigation";
+import Layout from "../Layouts/Layout";
+import { useSession } from "next-auth/react";
+import Loader from "../loader/Loader";
 
 interface Props {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function App({ children }: Props) {
-  const pathname = usePathname()
-  const { status } = useSession()
-  const excludePaths = ['/auth', '/admin']
-  const isStartWith = excludePaths.some((path) => pathname.startsWith(path))
+  const pathname = usePathname();
+  const { status } = useSession();
+  const excludePaths = ["/auth", "/admin"];
+  const isStartWith = excludePaths.some((path) => pathname.startsWith(path));
 
-  if (status === 'loading') {
-    return <Loader loading={status} />
+  if (status === "loading") {
+    return <Loader loading={status} />;
   }
 
-  return (
-    <>
-      {isStartWith ? (
-        <>{children}</>
-      ) : (
-        <Layout>
-          {children}
-        </Layout>
-      )
-      }
-    </>
-  )
+  return <>{isStartWith ? <>{children}</> : <Layout>{children}</Layout>}</>;
 }
