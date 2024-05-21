69b9a5905d74291a8f51f0d896acf40cadaf1dc7
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-05-21 20:18:44 +0100

    feat(app): Add action to delete project

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,6 +2,7 @@
 
 import prisma from "@/app/lib/prisma";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export async function toggleTestimonial({
   id,
@@ -32,4 +33,44 @@ export async function deleteTestimonial(id: string) {
   });
   revalidatePath("/admin/dashboard/testimonials");
   revalidatePath("/");
+}
+
+interface DeleteFormState {
+  error?: string;
+  success?: boolean;
+}
+
+export async function deleteProject(
+  id: string,
+  _formState: DeleteFormState,
+): Promise<DeleteFormState> {
+  if (!id) {
+    return {
+      error: "Invalid project id",
+    };
+  }
+
+  try {
+    const response = await prisma.project.delete({
+      where: {
+        id,
+      },
+    });
+
+    if (!response) {
+      return {
+        error: "Project not found",
+      };
+    }
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        error: error.message,
+      };
+    }
+  }
+
+  revalidatePath("/admin/dashboard/projects");
+  revalidatePath("/");
+  redirect("/admin/dashboard/projects");
 }
--- src/app/admin/dashboard/projects/[id]/page.tsx
+++ src/app/admin/dashboard/projects/[id]/page.tsx
@@ -1,119 +1,120 @@
-import React from 'react'
-import { CustomButton } from '@/components'
-import { BsGithub as GitHubIcon } from 'react-icons/bs';
-import { BiLinkExternal as LinkIcon } from 'react-icons/bi';
-import Image from 'next/image'
-import { getAuthSession } from '@/app/utils/auth';
-import ConditionalHeader from './ConditionalHeader';
-import DeleteProject from './DeleteProject';
-import Link from 'next/link';
-import { Metadata } from 'next';
+import React from "react";
+import { CustomButton } from "@/components";
+import { BsGithub as GitHubIcon } from "react-icons/bs";
+import { BiLinkExternal as LinkIcon } from "react-icons/bi";
+import Image from "next/image";
+import ConditionalHeader from "./ConditionalHeader";
+import DeleteProject from "./DeleteProject";
+import Link from "next/link";
+import { Metadata } from "next";
+import prisma from "@/app/lib/prisma";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 interface Props {
   params: {
-    id: string
-  }
+    id: string;
+  };
 }
 
 export const metadata: Metadata = {
-  title: 'Project Details',
-  description: 'Project details page',
-}
+  title: "Project Details",
+  description: "Project details page",
+};
 
 export async function generateStaticParams() {
-  const projects = await fetch(`${process.env.API_URL}/projects`).then(res => res.json())
+  const projects = await prisma.project.findMany({});
 
-  if (!projects) {
-    return null
-  }
-  return projects?.data?.map((project: any) => ({
+  return projects.map((project) => ({
     params: {
-      id: project.id
-    }
-  }))
+      id: project.id,
+    },
+  }));
 }
 
 const getProjectWithGeneratedParams = async (id: string) => {
-  const res = await fetch(`${process.env.API_URL}/projects/${id}`, {
-    next: {
-      revalidate: 10,
-    }
-  })
+  const res = await prisma.project.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      author: {
+        select: {
+          name: true,
+          email: true,
+        },
+      },
+    },
+  });
 
-  if (!res) {
-    return null
-  }
-  return res.json()
-}
+  return res;
+};
 
 const ProjectPage = async ({ params }: Props) => {
-  const { id } = params
-  const sessionData = await getAuthSession();
-  const response = getProjectWithGeneratedParams(id)
-  const [projectData, session] = await Promise.all([response, sessionData])
-  const project = projectData?.data
+  const { id } = params;
+  const project = await getProjectWithGeneratedParams(id);
 
   if (!project || project === null) {
     return (
-      <div className='w-full h-screen flex justify-center items-center'>
-        <h1 className='text-2xl font-semibold text-[var(--textColor)]'>Project not found</h1>
+      <div className="w-full h-screen flex justify-center items-center">
+        <h1 className="text-2xl font-semibold text-[var(--textColor)]">
+          Project not found
+        </h1>
       </div>
-    )
+    );
   }
 
   return (
     <>
-      <div className='w-[85%] h-full grid grid-cols-1 md:grid-cols-1 gap-1 p-5 my-[100px] mx-auto bg-[var(--contactBg)] rounded-lg'>
-        <div className='relative h-[408px] p-4 md:p-0'>
+      <div className="w-[85%] h-full grid grid-cols-1 md:grid-cols-1 gap-1 p-5 my-[100px] mx-auto bg-[var(--contactBg)] rounded-lg">
+        <div className="relative h-[408px] p-4 md:p-0">
           <Image
             src={project?.modalImgUrl!}
             alt={project?.modalImgUrl!}
             fill
-            className='w-full absolute object-cover rounded-lg'
+            className="w-full absolute object-cover rounded-lg"
           />
         </div>
-        <div className='flex flex-col p-3 gap-5'>
-          <div className='flex items-center justify-between'>
-            <h6 className='text-[var(--textColor)] text-lg font-semibold capitalize'>{project?.tag}</h6>
+        <div className="flex flex-col p-3 gap-5">
+          <div className="flex items-center justify-between">
+            <h6 className="text-[var(--textColor)] text-lg font-semibold capitalize">
+              {project?.tag}
+            </h6>
             <ConditionalHeader project={project} />
           </div>
-          <p className='text-lg font-normal mb-4'>
-            {project?.description}
-          </p>
-          <ul className='flex flex-wrap gap-2'>
-            Tech Stacks: {project?.stacks.map((stack: string, idx: number) => (
+          <p className="text-lg font-normal mb-4">{project?.description}</p>
+          <ul className="flex flex-wrap gap-2">
+            Tech Stacks:{" "}
+            {project?.stacks.map((stack: string, idx: number) => (
               <li key={idx}>{stack}</li>
             ))}
           </ul>
-          <div className='btn-container flex items-center justify-between w-full gap-5'>
-            <div className='flex items-center  gap-5'>
+          <div className="btn-container flex items-center justify-between w-full gap-5">
+            <div className="flex items-center  gap-5">
               {project?.liveUrl && (
-                <CustomButton
-                  variant='contained'
-                  color='primary'
-                  width='180px'
-                >
-                  <Link className='flex gap-3 items-center' href={project?.liveUrl} target='_blank'>
+                <CustomButton variant="contained" color="primary" width="180px">
+                  <Link
+                    className="flex gap-3 items-center"
+                    href={project?.liveUrl}
+                    target="_blank"
+                  >
                     Live Demo <LinkIcon />
                   </Link>
                 </CustomButton>
               )}
 
               {project?.githubUrl && (
-                <CustomButton
-                  variant='outlined'
-                  color='primary'
-                  width='180px'
-                >
-                  <Link className='flex gap-3 items-center' href={project?.githubUrl}>
+                <CustomButton variant="outlined" color="primary" width="180px">
+                  <Link
+                    className="flex gap-3 items-center"
+                    href={project?.githubUrl}
+                  >
                     Source Code <GitHubIcon />
                   </Link>
                 </CustomButton>
               )}
             </div>
-            <DeleteProject project={project} session={session} />
+            <DeleteProject project={project} />
           </div>
         </div>
       </div>
@@ -122,3 +123,4 @@ const ProjectPage = async ({ params }: Props) => {
 };
 
 export default ProjectPage;
+
