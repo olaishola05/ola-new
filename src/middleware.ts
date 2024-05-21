26593fce2b857c7bef8c64d26274d22aba63541c
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-05-21 21:25:46 +0100

    refactor(app): Refactor the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,27 @@
-import { NextResponse, NextRequest } from "next/server"
+import { NextResponse, NextRequest } from "next/server";
 
 const getCookieBasedOnEnv = (req: NextRequest) => {
-  const isProduction = process.env.NODE_ENV === 'production';
-  const cookie = isProduction ? req?.cookies?.getAll()?.find(cookie => cookie.name === '__Secure-next-auth.session-token') : req?.cookies?.getAll()?.find(cookie => cookie.name === 'next-auth.session-token') ?? false;
+  const isProduction = process.env.NODE_ENV === "production";
+  const cookie = isProduction
+    ? req?.cookies
+        ?.getAll()
+        ?.find((cookie) => cookie.name === "__Secure-next-auth.session-token")
+    : req?.cookies
+        ?.getAll()
+        ?.find((cookie) => cookie.name === "next-auth.session-token") ?? false;
 
   return cookie;
-}
+};
 
 export default async function middleware(req: NextRequest) {
   const isLoggedIn = getCookieBasedOnEnv(req);
-  const isOnAdminPage = req.nextUrl?.pathname.startsWith('/admin');
-  const isOnBlogWritePage = req.nextUrl?.pathname.startsWith('/blog/write');
-  const isOnBlogEditPage = req.nextUrl?.pathname.startsWith('/blog/edit');
+  const isOnAdminPage = req.nextUrl?.pathname.startsWith("/admin");
+  const isOnBlogWritePage = req.nextUrl?.pathname.startsWith("/blog/write");
+  const isOnBlogEditPage = req.nextUrl?.pathname.startsWith("/blog/edit");
 
   if ((isOnAdminPage || isOnBlogWritePage || isOnBlogEditPage) && !isLoggedIn) {
-    const loginUrl = new URL('/auths/signin', req?.nextUrl.origin);
-    loginUrl.searchParams.set('from', req.nextUrl.pathname);
+    const loginUrl = new URL("/auths/signin", req?.nextUrl.origin);
+    loginUrl.searchParams.set("from", req.nextUrl.pathname);
     return NextResponse.redirect(loginUrl);
   }
 
@@ -23,10 +29,5 @@ export default async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    '/admin/:path*',
-    '/blog/write',
-    '/blog/edit/:id(.*)'
-  ]
-}
-
+  matcher: ["/admin/:path*", "/blog/write", "/blog/edit/:id(.*)"],
+};
