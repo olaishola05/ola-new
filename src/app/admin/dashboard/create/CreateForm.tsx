bc37f456b0b548bcda4d4459274f74c6c15caeed
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-05-21 21:33:53 +0100

    refactor(app): updates

--- src/app/admin/dashboard/create/CreateForm.tsx
+++ src/app/admin/dashboard/create/CreateForm.tsx
@@ -1,54 +1,62 @@
+"use client";
 
-'use client';
-
-import React, { ReactNode, useCallback, useState } from 'react'
-import { sendDataToBackend } from '@/app/utils';
-import { useForm } from 'react-hook-form';
-import { ControllInput } from '@/components';
-import { Project } from '@/app/types';
-import { tailwindToast } from '@/components/Toast/Toast';
-
+import React, { ReactNode, useCallback, useState } from "react";
+import { sendDataToBackend } from "@/app/utils";
+import { useForm } from "react-hook-form";
+import { ControllInput } from "@/components";
+import { Project } from "@/app/types";
+import { tailwindToast } from "@/components/Toast/Toast";
 
 const InputBoxStyles = ({ children }: { children: ReactNode }) => (
-  <div className='flex gap-3'>
-    {children}
-  </div>
-)
+  <div className="flex gap-3">{children}</div>
+);
 
 export default function CreateForm() {
   const [responseOk, setResponseOk] = useState<boolean>(false);
-  const { register, handleSubmit, control, reset, formState: { isSubmitting } } = useForm<Project>({
-    mode: 'onBlur',
+  const {
+    register,
+    handleSubmit,
+    control,
+    reset,
+    formState: { isSubmitting },
+  } = useForm<Project>({
+    mode: "onBlur",
     defaultValues: {
-      name: '',
-      description: '',
-      githubUrl: '',
-      liveUrl: '',
+      name: "",
+      description: "",
+      githubUrl: "",
+      liveUrl: "",
       stacks: [],
-      coverImgUrl: '',
-      modalImgUrl: '',
-      tag: '',
+      coverImgUrl: "",
+      modalImgUrl: "",
+      tag: "",
     },
-  })
+  });
 
   const handleReset = useCallback(() => {
-    tailwindToast('info', 'Resetting form...', '', '')
+    tailwindToast("info", "Resetting form...", "", "");
     reset();
-  }, [reset])
+  }, [reset]);
 
   const onSubmit = async (data: Project) => {
     const newData: Project = {
       ...data,
-      stacks: data?.stacks?.toString().split(',').map((item: string) => item.trim()),
-    }
-    const res = await sendDataToBackend(newData, `${process.env.NEXT_PUBLIC_API_URL}`)
+      stacks: data?.stacks
+        ?.toString()
+        .split(",")
+        .map((item: string) => item.trim()),
+    };
+    const res = await sendDataToBackend(
+      newData,
+      `${process.env.NEXT_PUBLIC_API_URL}`,
+    );
     if (res?.status === 201) {
-      tailwindToast('success', `${res?.data.message}`, '', '')
-      setResponseOk(!responseOk)
+      tailwindToast("success", `${res?.data.message}`, "", "");
+      setResponseOk(!responseOk);
     } else {
-      tailwindToast('error', `Oops! ${res}`, '', '')
+      tailwindToast("error", `Oops! ${res}`, "", "");
     }
-  }
+  };
 
   React.useEffect(() => {
     if (responseOk) {
@@ -59,98 +67,99 @@ export default function CreateForm() {
   return (
     <>
       <form
-        className='w-full my-[50px] p-3 bg-[var(--bg)] mx-auto flex flex-col gap-3'
+        className="w-full my-[50px] p-3 bg-[var(--bg)] mx-auto flex flex-col gap-3"
         onSubmit={handleSubmit(onSubmit)}
       >
         <InputBoxStyles>
           <ControllInput
             control={control}
-            name='name'
-            placeholder='Project Name'
-            width={'100%'}
-            inputprops={register('name')}
+            name="name"
+            placeholder="Project Name"
+            width={"100%"}
+            inputprops={register("name")}
           />
           <ControllInput
             control={control}
-            name='tag'
-            placeholder='e.g frontend, backend, fullstack, etc'
-            size='small'
-            width={'100%'}
-            inputprops={register('tag')}
+            name="tag"
+            placeholder="e.g frontend, backend, fullstack, etc"
+            size="small"
+            width={"100%"}
+            inputprops={register("tag")}
           />
         </InputBoxStyles>
         <InputBoxStyles>
           <ControllInput
             control={control}
-            name='githubUrl'
-            placeholder='Github URL'
-            width={'100%'}
-            inputprops={register('githubUrl')}
+            name="githubUrl"
+            placeholder="Github URL"
+            width={"100%"}
+            inputprops={register("githubUrl")}
             required={false}
           />
 
           <ControllInput
             control={control}
-            name='liveUrl'
-            placeholder='Live URL'
-            width={'100%'}
-            inputprops={register('liveUrl')}
+            name="liveUrl"
+            placeholder="Live URL"
+            width={"100%"}
+            inputprops={register("liveUrl")}
             required={false}
           />
-
         </InputBoxStyles>
 
         <InputBoxStyles>
           <ControllInput
             control={control}
-            name='coverImgUrl'
-            placeholder='cover image url'
-            size='small'
-            width={'100%'}
-            inputprops={register('coverImgUrl')}
+            name="coverImgUrl"
+            placeholder="cover image url"
+            size="small"
+            width={"100%"}
+            inputprops={register("coverImgUrl")}
           />
 
           <ControllInput
             control={control}
-            name='modalImgUrl'
-            placeholder='modal image url'
-            size='small'
-            width={'100%'}
-            inputprops={register('modalImgUrl')}
+            name="modalImgUrl"
+            placeholder="modal image url"
+            size="small"
+            width={"100%"}
+            inputprops={register("modalImgUrl")}
           />
         </InputBoxStyles>
         <ControllInput
           control={control}
-          name='stacks'
-          placeholder='e.g React, Node, Express, MongoDB, etc'
-          size='small'
-          inputprops={register('stacks')}
+          name="stacks"
+          placeholder="e.g React, Node, Express, MongoDB, etc"
+          size="small"
+          inputprops={register("stacks")}
         />
 
         <ControllInput
           control={control}
-          placeholder='e.g Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-          size='small'
-          name='description'
-          type='textarea'
+          placeholder="e.g Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          size="small"
+          name="description"
+          type="textarea"
         />
 
-        <div className='w-full flex gap-4 justify-center mt-3'>
+        <div className="w-full flex gap-4 justify-center mt-3">
           <button
             type="submit"
             disabled={!register}
-            className='w-[30%] p-2 bg-[var(--cta)] text-[var(--ctaText)] rounded-md self-center text-lg hover:cursor-pointer'
+            className="w-[30%] p-2 bg-[var(--cta)] text-[var(--ctaText)] rounded-md self-center text-lg hover:cursor-pointer"
           >
-            {isSubmitting ? 'Submitting...' : 'Create'}
+            {isSubmitting ? "Submitting..." : "Create"}
           </button>
 
           <button
-            type='button'
-            className='w-[30%] p-2 bg-white text-[var(--primary)] rounded-md self-center text-lg border border-[var(--primary)] hover:cursor-pointer cancel'
+            type="button"
+            className="w-[30%] p-2 bg-white text-[var(--primary)] rounded-md self-center text-lg border border-[var(--primary)] hover:cursor-pointer cancel"
             onClick={() => handleReset()}
-          >Cancel</button>
+          >
+            Cancel
+          </button>
         </div>
       </form>
     </>
-  )
+  );
 }
