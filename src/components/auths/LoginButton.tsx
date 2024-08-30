f271e8b0606c70b0c1793d6beefaa3d2379b63bf
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-08-30 15:54:14 +0100

    refactor(app): Update button to take credential type

--- src/components/auths/LoginButton.tsx
+++ src/components/auths/LoginButton.tsx
@@ -3,8 +3,10 @@
 import React from 'react'
 import { signIn } from 'next-auth/react'
 import { BsGithub } from 'react-icons/bs'
+import { useFormStatus } from 'react-dom'
 
-export default function LoginButton({ from }: { from: string }) {
+export default function LoginButton({ from }: { from?: string }) {
+  const { pending } = useFormStatus()
   const handleLogin = async (provider: string) => {
     try {
       const res = await signIn(provider, {
@@ -21,8 +23,10 @@ export default function LoginButton({ from }: { from: string }) {
 
   return (
     <>
-      <button className='w-full h-10 md:w-[350px] justify-center flex gap-2 items-center text-base cursor-pointer px-2 py-3 border rounded-md shadow-sm md:text-base md:py-5 md:px-2 bg-black text-white' onClick={() => handleLogin('github')}>
-        Continue with
+      <button className='w-full h-10 md:w-[350px] justify-center flex gap-2 items-center text-base cursor-pointer px-2 py-3 border rounded-md shadow-sm md:text-base md:py-5 md:px-2 bg-black text-white' onClick={() => handleLogin('github')}
+        disabled={pending}
+      >
+        {pending ? 'Logging in..' : 'Continue with'}
         <BsGithub className='inline-block mr-2 md:mr-1 text-white' />
       </button></>
   )
