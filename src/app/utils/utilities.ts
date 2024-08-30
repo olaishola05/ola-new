ac5c113d9b321d80799d865b83ffb2fdadc7fc1c
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-08-30 16:02:40 +0100

    feat: Update UI & logics for regisration page

--- src/app/utils/utilities.ts
+++ src/app/utils/utilities.ts
@@ -240,3 +240,16 @@ export class Storage {
     localStorage && localStorage.removeItem(key);
   }
 }
+
+export interface PasswordTip {
+  text: string;
+  key: string;
+}
+
+export const passwordTips: PasswordTip[] = [
+  { text: "At least 8 characters", key: "length" },
+  { text: "At least 1 uppercase letter", key: "uppercase" },
+  { text: "At least 1 lowercase letter", key: "lowercase" },
+  { text: "At least 1 number", key: "number" },
+  { text: "At least 1 special character", key: "specialChar" },
+];
