5ba7ec9f5f058aaa97f911743987294849cfc396
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-09-12 18:54:34 +0100

    feat(app): Add loading state to button

--- src/components/auths/LoginButton.tsx
+++ src/components/auths/LoginButton.tsx
@@ -3,12 +3,12 @@
 import React from 'react'
 import { signIn } from 'next-auth/react'
 import { BsGithub } from 'react-icons/bs'
-import { useFormStatus } from 'react-dom'
 
 export default function LoginButton({ from }: { from?: string }) {
-  const { pending } = useFormStatus()
+  const [isLoading, setIsLoading] = React.useState(false)
   const handleLogin = async (provider: string) => {
     try {
+      setIsLoading(!isLoading)
       const res = await signIn(provider, {
         callbackUrl: from || '/',
       });
@@ -24,9 +24,9 @@ export default function LoginButton({ from }: { from?: string }) {
   return (
     <>
       <button className='w-full h-10 md:w-[350px] justify-center flex gap-2 items-center text-base cursor-pointer px-2 py-3 border rounded-md shadow-sm md:text-base md:py-5 md:px-2 bg-black text-white' onClick={() => handleLogin('github')}
-        disabled={pending}
+        disabled={isLoading}
       >
-        {pending ? 'Logging in..' : 'Continue with'}
+        {isLoading ? 'Logging in..' : 'Continue with'}
         <BsGithub className='inline-block mr-2 md:mr-1 text-white' />
       </button></>
   )
