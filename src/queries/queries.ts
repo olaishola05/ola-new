04013f61b6c499a043d7fb78f2f54ab61a208aa2
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-09-12 16:03:30 +0100

    refactor(app): Add revalidation for weekly refetch

--- src/queries/queries.ts
+++ src/queries/queries.ts
@@ -1,7 +1,6 @@
-import prisma from "@/lib/prisma";
 
 export async function getMediumPosts() {
-  const res = await fetch(`${process.env.MEDIUM_API_URL}`);
+  const res = await fetch(`${process.env.MEDIUM_API_URL}`, { next: { revalidate: 604800 } });
   if (!res.ok) {
     throw new Error("Something went wrong");
   }
