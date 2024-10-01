22c8eda55ab8ecca3dfa720c77df8c63664ac574
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-10-01 17:54:17 +0100

    feat(app): Add latest post as featured post

--- src/components/Posts/feature/Featured.tsx
+++ src/components/Posts/feature/Featured.tsx
@@ -1,21 +1,35 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { latestPost } from '@/app/lib'
 
-export default function Featured() {
+const getFeaturedPost = async () => {
+  const post = await latestPost()
+  return post
+}
+
+export default async function Featured() {
+  const post = await getFeaturedPost()
+
+  if (!post) {
+    return <div>No Featured Post.</div>
+  }
+
+  const { data } = post
   return (
     <div className='mt-8'>
       <h1 className='text-[32px] md:text-[64px] lg:text-[90px] font-light'>
-        <b>Hey, Ola here!</b> Discover my stories and creative ideas.
+        <b>Hey, Ola here!</b>
       </h1>
+      <h2 className='text-5xl md:text-7xl'>Discover my stories and creative ideas.</h2>
       <div className='mt-[60px] flex flex-col md:flex-row items-center gap-[50px]'>
         <div className='flex-1 h-[500px] relative'>
-          <Image src="/images/portrait-me.jpeg" alt="post" fill className='object-cover' />
+          <Image src={data.postImg} alt="post" fill className='object-cover rounded-lg' />
         </div>
         <div className='flex-1 flex flex-col gap-5'>
-          <h1 className='text-5xl'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </h1>
-          <p className='text-xl font-light text-softText'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore quidem sequi accusamus. Cupiditate in ratione, fugiat dolorem ipsa quos distinctio a nesciunt, laudantium laborum delectus ipsum iste. Molestias officia ex nemo.</p>
-          <Link href='/blog/posts/1' className='py-4 px-5 rounded-md border border-black max-w-max'>Read More</Link>
+          <h1 className='text-5xl'>{data.title} </h1>
+          <p className='text-xl font-light text-softText'>{data.description}</p>
+          <Link href={`/blog/posts/${data.slug}`} className='py-4 px-5 rounded-md border border-black max-w-max'>Read More</Link>
         </div>
       </div>
     </div>
