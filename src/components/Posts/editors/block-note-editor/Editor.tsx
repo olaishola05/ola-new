835876bd7d56aede983ef772f4f515cf370eeb4f
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-10-10 16:36:02 +0100

    feat(app): Update the markdown generator to take init content

--- src/components/Posts/editors/block-note-editor/Editor.tsx
+++ src/components/Posts/editors/block-note-editor/Editor.tsx
@@ -3,7 +3,7 @@ import dynamic from "next/dynamic";
 import { BlockNoteEditor, PartialBlock } from '@blocknote/core';
 
 interface EditorProps {
-  initialContent: PartialBlock[] | undefined | "loading"
+  initialContent: PartialBlock[]
   setInitialContent: (content: PartialBlock[]) => void
   setMarkdown: (markdown: string) => void
 }
@@ -13,7 +13,7 @@ export default function CustomEditor({ initialContent, setInitialContent, setMar
 
   const handleEditorContent = async (editor: BlockNoteEditor) => {
     setInitialContent(editor.document)
-    const markdown = await editor.blocksToMarkdownLossy(editor.document);
+    const markdown = await editor.blocksToMarkdownLossy(initialContent);
     setMarkdown(markdown)
   }
 
