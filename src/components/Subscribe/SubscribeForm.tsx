281ba1d9ffa38e115800e33f7b26dc5e7948052f
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-10-23 21:30:46 +0100

    refactor(app): Refactor the input box styles

--- src/components/Subscribe/SubscribeForm.tsx
+++ src/components/Subscribe/SubscribeForm.tsx
@@ -43,10 +43,10 @@ export default function SubscribeForm() {
   return (
     <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-4 w-full md:flex-row'>
       <label htmlFor="email" className='flex flex-col gap-1 w-full'>
-        <input type="email" placeholder="Enter your email" {...register('email')} className={`  p-3 border-none rounded-md bg-primary outline-none`} style={errors.email && { border: '1px solid red' }} />
-        {errors.email && <span className={errors && 'text-red-700 text-sm'}>{errors.email.message}</span>}
+        <input type="email" placeholder="Enter your email" {...register('email')} className={`p-3 rounded-md bg-primary outline-none bg-white`} style={errors.email && { border: '1px solid red' }} />
+        {errors.email && <span className={errors && 'text-sm text-ctaText'}>{errors.email.message}</span>}
       </label>
-      <button type="submit" className='flex max-w-max py-3 px-5 text-ctaText bg-cta rounded-lg self-center md:self-start border border-primary hover:bg-inherit hover:text-cta hover:border hover:border-cta disabled:bg-gray-500' disabled={isSubmitting}>Subscribe</button>
+      <button type="submit" className='flex max-w-max py-3 px-5 text-ctaText bg-cta rounded-lg self-center md:self-start border border-ctaText hover:bg-inherit hover:text-cta hover:border hover:border-cta disabled:bg-gray-500' disabled={isSubmitting}>Subscribe</button>
     </form>
   )
 }
