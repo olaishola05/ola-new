4b07bab184736f6354ac16e5901655162dbde110
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-10-23 21:28:30 +0100

    refactor(app): Update appearance for subcribe

--- src/components/Subscribe/Subscribe.tsx
+++ src/components/Subscribe/Subscribe.tsx
@@ -5,10 +5,14 @@ export default function Subscribe() {
   return (
     <div className='flex flex-col gap-5 mt-20 max-w-max mx-auto'>
       <h2 className='text-center text-xl font-medium text-softText'>Subscribe to my Newsletter!</h2>
-      <p>
-        Be the first to know about new posts and updates, subscribe to my newsletter now!
-      </p>
-      <SubscribeForm />
+      <div className='flex flex-col items-center justify-center gap-3 w-full px-2 py-8 md:py-16 bg-[#392467] rounded-md dark:bg-[#1f273a]'>
+        <p className='text-center text-white w-10/12'>
+          Be the first to know about new posts and updates, subscribe to my newsletter now!
+        </p>
+        <div className='w-10/12'>
+          <SubscribeForm />
+        </div>
+      </div>
     </div>
   )
 }
