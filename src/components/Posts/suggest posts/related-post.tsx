a9e1c261fba6da87a65b0940d7b69fd71e941d6c
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-10-25 21:43:02 +0100

    Create an add suggested posts to post page

---
+++ src/components/Posts/suggest posts/related-post.tsx
@@ -0,0 +1,32 @@
+import {fetchPublishedPosts} from "@/app/lib";
+import SuggestedPost from "@/components/Posts/suggest posts/suggested-post";
+
+const getPosts = async () => {
+    const posts = await fetchPublishedPosts()
+    return { posts }
+}
+
+function getTwoRandomItems<T>(arr: T[]): T[] {
+    const shuffled = arr.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, 2);
+}
+
+export default async function RelatedPosts({ slug }: { slug: string }) {
+    const {posts} = await getPosts()
+
+    if (!posts) {
+        return null
+    }
+    const filteredPosts = posts?.filter((post) => post?.data.slug != slug)
+    const data = getTwoRandomItems(filteredPosts)
+    return (
+        <div>
+            <h2 className='mb-2 text-2xl md:text-3xl text-softText'>Related Posts</h2>
+            <div className="flex flex-col md:flex-row gap-5 mb-[30px]">
+                {data.map((post, index) => (
+                   <SuggestedPost key={index} post={post?.data} />
+                ))}
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Posts/suggest posts/suggested-post.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+interface ItemData {
+        title: string,
+        date: string,
+        postImg?: string,
+        slug: string,
+}
+
+interface PostCardProps {
+    post: ItemData | undefined
+}
+
+export default function SuggestedPost({post}: PostCardProps) {
+    const {title, date, postImg, slug} = post || {}
+    console.log(post, 'post')
+
+    const fallbackImage = '/images/blogs/placeholder.jpeg'
+    const imageSrc = postImg && postImg !== 'undefined' && postImg.trim() !== ''
+        ? postImg
+        : fallbackImage
+
+    return (
+        <>
+            {imageSrc && (
+                <Link href={`/blog/posts/${slug}`}
+                      className='flex flex-col gap-4' key={slug}>
+                    <div className='relative h-[250px]'>
+                        <Image
+                            src={imageSrc}
+                            alt={title || 'Post Image'}
+                            className='object-cover rounded-lg w-full'
+                            priority
+                            fill
+                        />
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                        <div className=''>
+                            <span className='text-gray-400'>{date?.substring(0, 15)} </span>
+                        </div>
+                        <Link href={`/blog/posts/${slug}`} className='hover:text-cta'>
+                            <h1 className='text-base md:text-xl text-cta font-normal'>{title}</h1>
+                        </Link>
+                    </div>
+                </Link>
+            )}
+        </>
+    )
+}
