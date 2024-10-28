8ff86cea4978560e84af03d931e3fc85a9f73b1a
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-10-28 16:16:53 +0100

    Refactor: - Conditionally rendered logo and brand name using route path

--- src/components/Layouts/TopNav.tsx
+++ src/components/Layouts/TopNav.tsx
@@ -1,24 +1,29 @@
 'use client';
 
 import React from 'react';
-import { navItems, socialLinks, blogLinks } from '@/app/utils';
+import {blogLinks, navItems, socialLinks} from '@/app/utils';
 import Logo from './Logo';
-import { usePathname } from 'next/navigation';
+import {usePathname} from 'next/navigation';
 import ThemeToggle from '../ThemeToggle/ThemeToggle';
 import Link from 'next/link'
 import AdminRoutes from './AdminRoutes';
 import CustomIcon from '../IconsComponent/CustomIcon';
-import { ConditionalRoutes } from './ConditionalRoutes';
+import {ConditionalRoutes} from './ConditionalRoutes';
 
 export default function TopNav() {
   const routePath = usePathname();
   const isActive = (pathname: string) => routePath === pathname;
+  const isBlogPath = routePath.startsWith('/blog');
   return (
     <div className='flex items-center justify-between h-[80px] sticky top-0 z-10 bg-[var(--bg)]'>
       <div className='flex gap-1 flex-1 items-center'>
-        <Logo />
+          {isBlogPath ? (
+              <h1 className="md:hidden text-2xl font-bold text-[var(--primary)]">Code 'n' Beyond</h1>
+          ) : <Logo/>}
         <Link href='/'>
-          <h1 className='hidden md:block text-2xl font-bold text-[var(--primary)]'>Oladipupo Ishola</h1>
+          <h1 className='hidden md:block text-2xl font-bold text-[var(--primary)]'>
+              {isBlogPath ? "Code 'n' Beyond":'Oladipupo Ishola'}
+          </h1>
         </Link>
       </div>
       <div className='hidden lg:flex gap-3 flex-1'>
