324ba25bae3f6accc0246f7c98cb72d6f52d5eca
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-11-04 14:33:29 +0100

    Feat: Add loading state for editor while waiting for DOM to be Ready

--- src/components/Posts/editors/mdx-editor/editor.tsx
+++ src/components/Posts/editors/mdx-editor/editor.tsx
@@ -1,10 +1,35 @@
 'use client';
 
-import React, {forwardRef} from 'react';
+import React, {forwardRef, useEffect, useState} from 'react';
 import dynamic from "next/dynamic";
 import {MDXEditorMethods, MDXEditorProps} from "@mdxeditor/editor";
 
-const Editor = dynamic(() => import("./mdx-editor"),  { ssr: false });
+
+const Loading = () => {
+    const loadingStates: string[] = ['Loading', 'Preparaing the Editor', 'Wrapping up']
+    const [loadingIndex, setLoadingIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setLoadingIndex((prevIndex) => (prevIndex + 1) % loadingStates.length)
+        }, 1000)
+
+        return () => clearInterval(intervalId)
+    }, [])
+
+    return (
+        <div className="text-center">
+            {loadingStates.map((state, index) => (
+                <div key={index} style={{ display: index === loadingIndex ? 'block' : 'none' }}>
+                    {state}...
+                </div>
+            ))}
+        </div>
+    );
+}
+
+const Editor = dynamic(() => import("./mdx-editor"),
+    { ssr: false, loading: () => <Loading /> });
 
 export const CustomEditor = forwardRef<MDXEditorMethods, MDXEditorProps>((props, ref) =>
     <Editor
