2ff1aff3195f9345b93e2c195c1111ce90f68336
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-12-20 17:58:04 +0100

    Update cats styles

--- src/components/Menu/Cats.tsx
+++ src/components/Menu/Cats.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import React from 'react'
-import {usePathname} from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import Link from 'next/link'
-import {Category} from '@prisma/client';
-import {mostUsedTags} from "@/components/Posts";
+import { Category } from '@prisma/client';
+import { mostUsedTags } from "@/components/Posts";
 
 export default function Cats({ categories }: { categories: Category[] }) {
   const pathname = usePathname()
@@ -17,19 +17,23 @@ export default function Cats({ categories }: { categories: Category[] }) {
   const cats: string[] = mostUsedTags(categories)
   return (
     <>
-        {cats.length > 0 && (
-            <>
-                <h2 className='text-gray-500 text-base font-normal'>Discover by topic</h2>
-                <h1 className='text-3xl'>Categories</h1>
-                <div className='grid grid-cols-2 gap-2 mt-3'>
-                    {cats?.map((title: string, index: number) => (
-                        <Link href={`/blog/posts?cat=${title}`} key={index}>
-                            {title}
-                        </Link>
-                    ))}
-                </div>
-            </>
-        )}
+      {cats.length > 0 && (
+        <>
+          <h2 className='text-gray-500 text-base font-normal'>Discover by topic</h2>
+          <h1 className='text-3xl'>Categories</h1>
+          <div className='w-1/2 grid grid-cols-3 gap-1 mt-3'>
+            {cats?.map((title: string, index: number) => (
+              <Link
+                href={`/blog/posts?cat=${title}`}
+                key={index}
+                className='text-lg font-semibold text-textColor'
+              >
+                {title}
+              </Link>
+            ))}
+          </div>
+        </>
+      )}
     </>
   )
 }
