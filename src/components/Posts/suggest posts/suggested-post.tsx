38374ae9fb22557fddb8d7372f2e21d37ffdf065
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-12-20 18:09:19 +0100

    refactor(app): Update related posts image size

--- src/components/Posts/suggest posts/suggested-post.tsx
+++ src/components/Posts/suggest posts/suggested-post.tsx
@@ -3,48 +3,45 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 interface ItemData {
-        title: string,
-        date: string,
-        postImg?: string,
-        slug: string,
+  title: string,
+  date: string,
+  postImg?: string,
+  slug: string,
 }
 
 interface PostCardProps {
-    post: ItemData | undefined
+  post: ItemData | undefined
 }
 
-export default function SuggestedPost({post}: PostCardProps) {
-    const {title, date, postImg, slug} = post || {}
+export default function SuggestedPost({ post }: PostCardProps) {
+  const { title, date, postImg, slug } = post || {}
 
-    const fallbackImage = '/images/blogs/placeholder.jpeg'
-    const imageSrc = postImg && postImg !== 'undefined' && postImg.trim() !== ''
-        ? postImg
-        : fallbackImage
+  const fallbackImage = '/images/blogs/placeholder.jpeg'
+  const imageSrc = postImg && postImg !== 'undefined' && postImg.trim() !== ''
+    ? postImg
+    : fallbackImage
 
-    return (
-        <>
-            {imageSrc && (
-                <Link href={`/blog/posts/${slug}`}
-                      className='flex flex-col' key={slug}>
-                    <div className='relative h-[250px]'>
-                        <Image
-                            src={imageSrc}
-                            alt={title || 'Post Image'}
-                            className='object-cover rounded-lg w-full'
-                            priority
-                            fill
-                        />
-                    </div>
-                    <div className='flex flex-col gap-2'>
-                        <div className=''>
-                            <span className='text-gray-400'>{date?.substring(0, 15)} </span>
-                        </div>
-                        <Link href={`/blog/posts/${slug}`} className='hover:text-cta'>
-                            <h1 className='text-base md:text-xl text-cta font-normal'>{title}</h1>
-                        </Link>
-                    </div>
-                </Link>
-            )}
-        </>
-    )
+  return (
+    <>
+      {imageSrc && (
+        <Link href={`/blog/posts/${slug}`}
+          className='flex flex-col w-[350px]' key={slug}>
+          <div className='relative h-[250px] w-full'>
+            <Image
+              src={imageSrc}
+              alt={title || 'Post Image'}
+              className='object-cover rounded-lg w-full'
+              priority
+              fill
+            />
+          </div>
+          <div className='flex flex-col gap-2'>
+            <Link href={`/blog/posts/${slug}`} className='hover:text-cta'>
+              <h2 className='text-base md:text-xl text-cta font-semibold'>{title}</h2>
+            </Link>
+          </div>
+        </Link>
+      )}
+    </>
+  )
 }
