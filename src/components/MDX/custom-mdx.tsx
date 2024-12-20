d692a912e1c5558c09008654c8286a4d43900b7d
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-12-20 15:58:06 +0100

    Temporary add posts to git ignore

--- src/components/MDX/custom-mdx.tsx
+++ src/components/MDX/custom-mdx.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { MDXRemote } from 'next-mdx-remote/rsc'
+import {MDXRemote} from 'next-mdx-remote/rsc'
 import remarkGfm from 'remark-gfm'
 import rehypeSlug from 'rehype-slug'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+// @ts-ignore
 import remarkA11yEmoji from '@fec/remark-a11y-emoji'
 import remarkToc from 'remark-toc'
-import { mdxComponents } from './markdown-components'
+import {mdxComponents} from './markdown-components'
 import MdxLayout from './mdx-layout'
 import rehypePrettyCode from 'rehype-pretty-code';
 
