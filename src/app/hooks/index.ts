ef4b33574e44944a8827d536d8b75f805b2fcf7a
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-12-23 19:16:08 +0100

    feat(app): higlighter for table of contents hook

--- src/app/hooks/index.ts
+++ src/app/hooks/index.ts
@@ -15,3 +15,5 @@ export {
   usePostTitle,
 };
 
+export { useHighlighted } from './useHighlighted'
+
---
+++ src/app/hooks/useHighlighted.ts
@@ -0,0 +1,30 @@
+import { useEffect, useRef, useState } from "react";
+
+export function useHighlighted(id: string) {
+  const observer = useRef<IntersectionObserver | null>(null);
+  const [activeId, setActiveId] = useState("");
+
+  useEffect(() => {
+    const handleObserver = (entries: any[]) => {
+      entries.forEach((entry) => {
+        if (entry?.isIntersecting) {
+          setActiveId(entry.target.id);
+        }
+      });
+    };
+
+    observer.current = new IntersectionObserver(handleObserver, {
+      rootMargin: "0% 0% -35% 0px",
+    });
+
+    const elements = document.querySelectorAll("h2, h3, h4");
+    elements.forEach((elem) => observer.current && observer.current.observe(elem));
+    return () => {
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+    };
+  }, []);
+  const highlighted = activeId === id;
+  return [highlighted, setActiveId];
+}
