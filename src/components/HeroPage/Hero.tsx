632f407ab23fa37a72498ec1b855450c9b6a2e38
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-12-27 17:26:54 +0100

    refactor(app): Redesign the hero section layout

--- src/components/HeroPage/Hero.tsx
+++ src/components/HeroPage/Hero.tsx
@@ -4,18 +4,19 @@ import Companies from "./Companies";
 import { socialLinks } from "@/app/utils";
 import { Icons } from "..";
 import HeroTitleAnimation from "./greeting";
+import Image from "next/image";
 
 export default function HeroSection() {
   return (
     <main
-      className="flex p-2 w-full h-full mt-8 mx-auto gap-10 relative md:flex-row md:py-0 md:px-5 md:h-[50vh] lg:h-[70vh] lg:flex-col lg:mt-20 lg:mb-0 lg:mx-auto md:w-[90%] lg:w-[100%] md:gap-8 mb-10"
+      className="flex flex-col-reverse md:flex-row p-2 w-full h-full mt-8 mx-auto gap-10 relative md:py-0 md:px-5 md:h-[50vh] lg:h-[75vh] lg:mt-20 lg:mb-0 lg:mx-auto md:w-[90%] lg:w-[100%] md:gap-8 mb-10"
       data-aos="fade-up"
       data-aos-duration="3000"
     >
-      <div className="w-full flex flex-col gap-4 md:gap-6 lg:gap-7">
+      <div className="w-full md:flex-1 flex flex-col gap-4 md:gap-6 lg:gap-7 md:py-20">
         <HeroTitleAnimation />
         <p
-          className="intro self-center w-full text-justify md:text-center md:w-[600px] lg:w-[900px] font-light text-base md:text-lg lg:text-xl text-[var(--textColor)]"
+          className="intro self-center md:self-start w-full text-justify md:text-left md:w-[700px] font-light text-base md:text-lg lg:text-xl text-[var(--textColor)]"
           data-aos="zoom-in-up"
           data-aos-duration="8000"
         >
@@ -23,7 +24,7 @@ export default function HeroSection() {
           businesses & startups to develop accessible, human-centered products
           that meet their {"customers'"} needs.
         </p>
-        <div className="self-center flex justify-center gap-5 w-full mt-6">
+        <div className="self-center md:self-start flex justify-center md:justify-start gap-5 w-full mt-6">
           <Link
             href="/#portfolio"
             className="w-[200px] h-[45px] px-3 py-5 rounded-full leading-4 tracking-tighter shadow-lg flex items-center justify-center gap-10 text-base md:text-lg text-[var(--cta)] bg-inherit border border-[var(--primary)] hover:bg-[var(--cta)] hover:text-[var(--ctaText)] hover:border hover:border-[var(--cta)]"
@@ -47,7 +48,10 @@ export default function HeroSection() {
             </div>
           ))}
         </div>
-        <Companies />
+        {/* <Companies /> */}
+      </div>
+      <div className="w-full md:flex-1">
+        <Image src="/images/ola.jpeg" alt="Hero Image" width={1024} height={1024} loading="lazy" className="h-[36rem] w-full md:w-11/12 rounded-xl object-cover" />
       </div>
     </main>
   );
