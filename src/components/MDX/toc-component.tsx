40e857623746f87e99ed39d5327c0aa9d3b60390
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-12-31 15:49:29 +0100

    refactor(app): contionally render TOC title if nodes > 0

--- src/components/MDX/toc-component.tsx
+++ src/components/MDX/toc-component.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import React from 'react'
 
 interface TableOfContentsProps {
-  nodes: any;
+  nodes: Node[];
 }
 
 interface Node {
@@ -40,9 +40,9 @@ const TOCLink = ({ node }: { node: Node }) => {
 
 function renderNodes(nodes: any[]) {
   return (
-    <ul>
+    <ul className='pl-4'>
       {nodes.map((node) => (
-        <li key={node.data.hProperties.id}>
+        <li key={node.data.hProperties.id} className='flex gap-2'>
           <TOCLink node={node} />
           {node.children?.length > 0 && renderNodes(node.children)}
         </li>
@@ -52,12 +52,13 @@ function renderNodes(nodes: any[]) {
 }
 
 export default function TableOfContents({ nodes }: TableOfContentsProps) {
-  if (nodes.lenght <= 0) {
+  if (nodes.length <= 0) {
     return null
   }
+
   return (
     <div className='hidden md:block w-full'>
-      <h3 className='text-3xl mb-3'>{nodes.lenght && "Table of contents"}</h3>
+      <h3 className='text-3xl mb-3'>{nodes.length > 0 && "Table of contents"}</h3>
       {renderNodes(nodes)}
     </div>
   )
