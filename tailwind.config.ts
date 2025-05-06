86c45f2728a524079a5b62591915b0463645f4e0
Oladipupo Ishola <olaishola@hotmail.co.uk>
2025-05-06 19:33:43 +0100

    feat(app): Add shadcn to package

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,92 +1,110 @@
 import type {Config} from 'tailwindcss'
 
 const config: Config = {
-  content: [
+    darkMode: ['class'],
+    content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        bg: 'var(--bg)',
-        textColor: 'var(--textColor)',
-        textDark: 'var(--textColorDark)',
-        softBg: 'var(--softBg)',
-        softText: 'var(--softTextColor)',
-        pry: 'var(--primary)',
-        contactBg: 'var(--contactBg)',
-        cta: 'var(--cta)',
-        ctaText: 'var(--ctaText)',
-        formText: 'var(--formText)',
-        btnMode: 'var(--btnMode)',
-        contact: 'var(--contact)',
-        contactText: 'var(--contactText)',
-        editorBg: 'var(--editorBg)',
-        editorTop: 'var(--editorTop)',
-
-        // Editor colors
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-        borderRadius: {
-          lg: "var(--radius)",
-          md: "calc(var(--radius) - 2px)",
-          sm: "calc(var(--radius) - 4px)",
-        },
-        keyframes: {
-          "accordion-down": {
-            from: { height: "0" },
-            to: { height: "var(--radix-accordion-content-height)" },
-          },
-          "accordion-up": {
-            from: { height: "var(--radix-accordion-content-height)" },
-            to: { height: "0" },
-          },
-        },
-        animation: {
-          "accordion-down": "accordion-down 0.2s ease-out",
-          "accordion-up": "accordion-up 0.2s ease-out",
-        },
-
-      },
-      flex: {
-        '2': '2 2 0%',
-        '3': '3 3 0%',
-        '4': '4 4 0%',
-        '5': '5 5 0%',
-      },
-    },
+  	extend: {
+  		colors: {
+  			bg: 'var(--bg)',
+  			textColor: 'var(--textColor)',
+  			textDark: 'var(--textColorDark)',
+  			softBg: 'var(--softBg)',
+  			softText: 'var(--softTextColor)',
+  			pry: 'var(--primary)',
+  			contactBg: 'var(--contactBg)',
+  			cta: 'var(--cta)',
+  			ctaText: 'var(--ctaText)',
+  			formText: 'var(--formText)',
+  			btnMode: 'var(--btnMode)',
+  			contact: 'var(--contact)',
+  			contactText: 'var(--contactText)',
+  			editorBg: 'var(--editorBg)',
+  			editorTop: 'var(--editorTop)',
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			borderRadius: {
+  				lg: 'var(--radius)',
+  				md: 'calc(var(--radius) - 2px)',
+  				sm: 'calc(var(--radius) - 4px)'
+  			},
+  			keyframes: {
+  				'accordion-down': {
+  					from: {
+  						height: '0'
+  					},
+  					to: {
+  						height: 'var(--radix-accordion-content-height)'
+  					}
+  				},
+  				'accordion-up': {
+  					from: {
+  						height: 'var(--radix-accordion-content-height)'
+  					},
+  					to: {
+  						height: '0'
+  					}
+  				}
+  			},
+  			animation: {
+  				'accordion-down': 'accordion-down 0.2s ease-out',
+  				'accordion-up': 'accordion-up 0.2s ease-out'
+  			},
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		flex: {
+  			'2': '2 2 0%',
+  			'3': '3 3 0%',
+  			'4': '4 4 0%',
+  			'5': '5 5 0%'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography"), require('@tailwindcss/typography')],
 } satisfies Config;
