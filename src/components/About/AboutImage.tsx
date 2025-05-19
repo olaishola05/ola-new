42a48cf745f29786ad133ceea8ab417541f1747b
Oladipupo Ishola <olaishola@hotmail.co.uk>
2025-05-19 19:10:26 +0100

    refactor(app): Change about img & structure

--- src/components/About/AboutImage.tsx
+++ src/components/About/AboutImage.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Image from "next/image";
-import { socialLinks } from "@/app/utils";
-import Icons from "../IconsComponent/Icons";
 
 interface AboutImage {
   photo: string;
@@ -11,24 +9,19 @@ const AboutImage = (props: AboutImage) => {
   const { photo } = props;
   return (
     <div
-      className="w-[10%] md:hidden lg:block lg:w-80 shadow[rgb(209, 217, 230) 5px 5px 15px 0px, rgb(255, 255, 255) -5px -5px 15px 0px] rounded-t-full"
+      className="w-[35%] py-5"
       data-aos="fade-up"
     >
       <Image
         src={photo}
         alt="Ola"
-        width={300}
-        height={400}
-        className="w-full object-fill z-10 rounded-t-full transition-all duration-300 ease-in-out"
+        priority
+        width={100}
+        height={300}
+        className="w-full object-cover transition-all duration-300 ease-in-out rounded-md shadow-xl hover:scale-105"
       />
-      <div className="flex justify-around w-80 my-3">
-        {socialLinks.map((link) => (
-          <Icons link={link} key={link.id} />
-        ))}
-      </div>
     </div>
   );
 };
 
 export default AboutImage;
-
