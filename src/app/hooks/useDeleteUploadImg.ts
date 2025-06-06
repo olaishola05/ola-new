00ff28566c0d8930bea01c92b8ceeb936ac900a5
Oladipupo Ishola <olaishola@hotmail.co.uk>
2025-06-06 12:49:10 +0100

    feat(app): Update hook to handle deletion for multiple route

--- src/app/hooks/useDeleteUploadImg.ts
+++ src/app/hooks/useDeleteUploadImg.ts
@@ -1,21 +1,81 @@
+// "use client";
+
+// export const useDeleteUploadImg = async (publicId: string) => {
+//   try {
+//     const response = await fetch(`/api/v1/delete-image`, {
+//       method: 'DELETE',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ publicId }),
+//     });
+
+//     if (!response.ok) {
+//       throw new Error('Failed to delete image');
+//     }
+//     return response;
+//   } catch (error) {
+//     console.error('Delete error:', error);
+//     throw error;
+//   }
+// };
+
+
+// hooks/useDeleteUploadImg.ts
+// "use client";
+
+// export const useDeleteUploadImg = () => {
+//   return async (publicId: string) => {
+//     try {
+//       const response = await fetch(`/api/v1/delete-image`, {
+//         method: 'DELETE',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ publicId }),
+//       });
+
+//       if (!response.ok) {
+//         throw new Error('Failed to delete image');
+//       }
+//       return response;
+//     } catch (error) {
+//       console.error('Delete error:', error);
+//       throw error;
+//     }
+//   };
+// };
+
+
 "use client";
 
-export const useDeleteUploadImg = async (publicId: string) => {
-  try {
-    const response = await fetch(`/api/v1/delete-image`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ publicId }),
-    });
+export const useDeleteUploadImg = () => {
+  return async ({
+    publicId,
+    projectId,
+    type,
+  }: {
+    publicId: string;
+    projectId?: string;
+    type?: "cover" | "image";
+  }) => {
+    const isEdit = projectId && type;
+    const endpoint = isEdit ? "/api/v1/delete-db-image" : "/api/v1/delete-cloud-image";
+    try {
+      const response = await fetch(endpoint, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ publicId, projectId, type }),
+      });
 
-    if (!response.ok) {
-      throw new Error('Failed to delete image');
+      if (!response.ok) throw new Error("Failed to delete image");
+
+      return response;
+    } catch (error) {
+      console.error("Image delete hook error:", error);
+      throw error;
     }
-    return response;
-  } catch (error) {
-    console.error('Delete error:', error);
-    throw error;
-  }
+  };
 };
